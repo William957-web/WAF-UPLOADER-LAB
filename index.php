6660cd7c52e8e1bb5cc67fb3d9a1af89bf70975d
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-10 22:04:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Your File</title>
+</head>
+<body>
+    <h2>Upload Your File</h2>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload"></br></br></br>
+	<input type="text" name="fileName" id="fileName" placeholder="filename" style="width:200px"></br></br></br>
+        <input type="submit" value="Upload Image" name="submit"></br></br></br>
+    </form>
+</body>
+</html>
