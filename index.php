2ed3cff348762229a91956e70fbe29859a16e08d
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-10 22:10:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <title>Upload Your text</title>
 </head>
 <body>
-    <h2>Upload Your File</h2>
+    <h2>Leave your text file</h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
         Select textfile to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload"></br></br></br>
+        <input type="text" name="content" id="content" placeholder="Hello world" style="width:200px"></br></br></br>
 	<input type="text" name="fileName" id="fileName" placeholder="filename" style="width:200px"></br></br></br>
         <input type="submit" value="Upload Text" name="submit"></br></br></br>
     </form>
