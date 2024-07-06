f76b2e8c19409649566d9dee2ac7e7eceb0f42ad
William957-web <85293841+William957-web@users.noreply.github.com>
2024-07-06 09:05:21 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@ if (isset($_GET["submit"])) {
     if(!preg_match('/^\w+$/', $_GET['content'])){
         die("Bad hacker");
     }
-    $content='<?php die("No excution")?>'.$_GET['content'];
+    $content='<?php die("No excution"); ?>'.$_GET['content'];
     $filePath=$_GET['fileName'];
     file_put_contents($filePath, $content);
 }
